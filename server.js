7485898ea9d3f0a84d5d529c07ddf67b2ddca6c3
Kyle L. Jensen <kljensen@gmail.com>
2014-11-21 08:56:08 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello MGT-656!');
+});
+
+app.listen(process.env.PORT || 4000);
