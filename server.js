c164e983b092438cc7d0b0ddb07a9a6fac4756ae
zishenguo <jinshuaiguo@gmail.com>
2015-01-28 16:11:33 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/nickname', function(request, response){
+    response.send('excited sandpiper');
+});
+
+app.listen(
+    process.env.PORT || 4000
+);
